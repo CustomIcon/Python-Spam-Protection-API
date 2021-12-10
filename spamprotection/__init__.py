0db79b72d69cf96aba792c722518ee6cc023f244
Justin Crawford <Justin@stacksmash.net>
2021-12-09 16:59:38 -0800

    Fix a dependency problem when installing on fresh installs

--- spamprotection/__init__.py
+++ spamprotection/__init__.py
@@ -1,4 +1,9 @@
 """Initial Directory."""
-from .client import SPBClient  # noqa: F401
-
 __version__ = "0.1.1"
+
+try:
+	from .client import SPBClient  # noqa: F401
+except ModuleNotFoundError:
+	# in setup.py, this import will cause problems
+	# because dependencies aren't always installed
+	pass
